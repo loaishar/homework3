c2800cfd8273fd69c73b3f048824f6d9d64a7a95
loaishar <55962133+loaishar@users.noreply.github.com>
2021-12-15 23:24:11 +0200

    commit all

--- main.c
+++ main.c
@@ -1,70 +1,133 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define WORD 8
 #define TXT 8
-
-
-
-void func_A(char str[],char input[])
+int value_of_char_geo(char c)
 {
-   int  sum_for_word=0;
-   char ch;
-        char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};  
-        for (int i=0; i<31; i++){
-            ch=input[i];
-            for (int j = 0; j < 26; j++)
+    char letters_alphapet[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    for (int j = 0; j < 52; j++)
+    {
+        if (j < 26)
+        {
+            if(letters_alphapet[j]==c)
             {
-                if(ch==letters[j])
-                sum_for_word+=j+1;
+                 
+                return j+1;
             }
-            
         }
-        printf("the sum is : %d",sum_for_word);
-        
-        
-        
+        else
+        {
+            if(letters_alphapet[j]==c)
+            {
+                return j-25;
+            }
+        }
+    }
+    return 0;
+}
+void print_text_from_i_to_j(char* print_text,int i, int j){
+for (int from = i; from < j; from++)
+{
+    printf("%c",print_text[from]);
+}
+printf("\n");
+
+}
+
+int text_length_geo(char *str_new)
+{
+    int sum_for_word = 0;
+    char ch;
+    char letters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int j = 0;
+    for (int i = j; str_new[i] != '\0'; i++)
+    {
+        for( j = 0; sum_for_word<=8; j++)
+        {
+          sum_for_word+=value_of_char_geo(str_new[j]);
+
+        }
+        print_text_from_i_to_j(str_new,i,j);
+        i=j;
+    }
+    
+}
+
+int word_length_geo(char *str_new2)
+{
+    int sum_for_word = 0;
+    char ch;
+    char letters[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    for (int i = 0; i < 30; i++)
+    {
+        ch = *(str_new2 + i);
+        if (!(ch))
+            break;
+        for (int j = 0; j < 52; j++)
+        {
+            if (j < 26)
+            {
+                if (ch == letters[j])
+                {
+                    sum_for_word += j + 1;
+                    break;
+                }
+            }
+            else
+            {
+                if (ch == letters[j])
+                {
+                    sum_for_word += j - 25;
+                    break;
+                }
+            }
+        }
+    }
+    return sum_for_word;
+}
+char *new_words(int newsize)
+{
+
+    char *str = (char *)malloc((newsize + 1) * sizeof(char));
+    int i;
+    char c;
+    for (i = 0; i <= newsize; i++)
+    {
+        // scanf("");
+
+        if (i == newsize)
+        {
+            break;
+        }
+        else
+        {
+            str[i] = c;
+        }
+    }
+    str[newsize] = '\0'; /* terminate the string */
+
+    return str;
 }
 
 int main(void)
 {
-    char str[TXT + 1]; /* allocate one more element for terminating null-character */
-    int i;
-    char c;
-
     printf("Insert str: MAX 8 CHARS!\n\n");
-    for(i = 0; i <= TXT; i++){
-        c = getchar();
-
-        if(i == TXT){
-            break;
-        }
-        else{
-            str[i] = c;
-        }
-    }
-    str[TXT] = '\0'; /* terminate the string */
-
-   // printf("%s\n", str);
-//hi there i am loai
-    char word_input[WORD + 1]; 
-    int n;
-    char m;
+    char string1[TXT];
+    scanf("%8s", string1);
+    // for (int i = 0; i < 8; ++i) {
+    //      printf("%c, ",string1[i]);
+    //  }
 
     printf("Insert word_input: MAX 8 CHARS!\n\n");
-    for(n = 0; n <= WORD; n++){
-        m = getchar();
+    char string2[WORD];
+    scanf("%8s", string2);
 
-        if(n == WORD){
-            break;
-        }
-        else{
-            word_input[n] = m;
-        }
-    }
-    word_input[WORD] = '\0';
-
-    //printf("%s\n", word_input);
-    func_A(str,word_input);
-
-    
+    //  for (int i = 0; i < 8; ++i) {
+    //      printf("%c, ",string2[i]);
+    //  }
+    // text_length_geo(string1);
+    int x = word_length_geo(string2);
+    printf("\n%d\n", x);
+    text_length_geo(string1);
 }
-
